94157cdc8342a88f06a60d0541d06ca646d153df
Beepbloop <louisliu2096@gmail.com>
2020-09-29 16:53:28 -0400

    basic data cleaning

--- Find_Ground_Truth.py
+++ Find_Ground_Truth.py
@@ -1,10 +1,25 @@
 from Utilities import *
 
+def clean_lane(dots):
+    start = bool(dots[0][0] - dots[1][0] > 0)
+
+    for i in range(1, len(dots)):
+        if bool(dots[i-1][0] - dots[i][0] > 0) != start:
+            dots = dots[:i]
+            break
+    return dots
+
 video_num = '0250'
 vid = cv.VideoCapture(get_video(video_num))
 
 factor = 2
-for i in range(1, 10000):
+start_frame = 500
+for i in range(1, start_frame):
+    sucess, temp = vid.read()
+    if not sucess:
+        exit()
+
+for i in range(start_frame, 700):
     sucess, orignal_frame = vid.read()
     if not sucess:
         print(i)
@@ -14,8 +29,14 @@ for i in range(1, 10000):
     img = orignal_frame.copy()
     img = cv.resize(img, (0, 0), fx=1 / factor, fy=1 / factor)
     lanes = get_lanes(video_num, str(i), factor)
-    doted = draw_lanes(img, lanes)
+    cleaned_lanes = []
+    for i in range(len(lanes)):
+        cleaned_lanes.append(clean_lane(lanes[i]))
+    # lanes = clean_lane(lanes)
+    orignal = draw_lanes(img, lanes)
+    cleaned = draw_lanes(img, cleaned_lanes)
 
-    cv.imshow('Video_{}'.format(video_num), doted)
+    cv.imshow('Video_{}_orignal'.format(video_num), orignal)
+    cv.imshow('Video_{}_cleaned'.format(video_num), cleaned)
     cv.waitKey(0)
 vid.release()
